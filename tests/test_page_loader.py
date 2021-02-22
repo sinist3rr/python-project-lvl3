aa0f8c0250f589798a1ccf376b69e76c0b25c713
sinist3rr <darkfloki@protonmail.com>
2021-02-22 17:06:41 +0200

    fix tests

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -7,27 +7,31 @@ import page_loader
 
 
 @pytest.mark.parametrize(
-    'url,output_dir,result',
+    'url,output_dir,result_line,result',
     [
         (
             'http://example.com',
             '/tmp/',
+            '/tmp/example-com.html',
             'example-com'
         ),
         (
             'https://ru.hexlet.io/courses',
             '/tmp/',
+            '/tmp/ru-hexlet-io-courses.html',
             'ru-hexlet-io-courses'
         ),
         (
             'http://example.com/index.html',
             '/tmp/',
+            '/tmp/example-com-index.html',
             'example-com-index'
         )]
 )
-def test_page_loader(url, output_dir, result):
+def test_page_loader(url, output_dir, result_line, result):
     filepath = 'tests/fixtures/{}.html'.format(result)
-    with open(filepath) as fp:
-        result_line = fp.read()
+    # with open(filepath) as fp:
+    # result_line = fp.read()
 
+    # assert page_loader.download(url, output_dir) == result_line
     assert page_loader.download(url, output_dir) == result_line
