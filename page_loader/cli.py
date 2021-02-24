13d666056339d06f30ec966dc66bc2599bd832e3
sinist3rr <darkfloki@protonmail.com>
2021-02-24 19:22:07 +0200

    fix arguments

--- page_loader/cli.py
+++ page_loader/cli.py
@@ -5,10 +5,15 @@ import os
 def prompt_args():
     parser = argparse.ArgumentParser(prog='page-loader',
                                      usage='%(prog)s [options] <url>',
-                                     description='download html page')
-    parser.add_argument('-o', '--output [dir]',
-                        dest='OUTPUT', default=os.getcwd(),
-                        help="output dir")
+                                     description='download html page',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter  # noqa: E501
+                                     )
+    parser.add_argument('-o',
+                        '--output [dir]',
+                        default=os.getcwd(),
+                        help="output dir",
+                        dest='OUTPUT'
+                        )
     parser.add_argument(dest='URL')
     parser._optionals.title = "Options"
     return parser
