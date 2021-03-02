9548398b20f103afef289b36f7e873e624f8aac9
sinist3rr <darkfloki@protonmail.com>
2021-03-02 17:12:37 +0200

    refactoring

--- page_loader/download_lib.py
+++ page_loader/download_lib.py
@@ -27,6 +27,22 @@ def format_url(url: str, out_type: str) -> str:
         return 'Wrong type - {}'.format(out_type)
 
 
+def download_img(URL, OUTPUT_DIR, image_tags):
+    dir_name = format_url(URL, 'dir')
+    dir_full_path = os.path.join(OUTPUT_DIR, dir_name)
+    if not os.path.exists(dir_full_path):
+        os.mkdir(dir_full_path)
+
+    for image_link in image_tags:
+        link = urljoin(URL, image_link.get('src'))
+        response = requests.get(link)
+        image_name = format_url(link, 'file')
+        full_file_path = '{}/{}'.format(dir_full_path, image_name)
+        image_link['src'] = full_file_path
+        with open(full_file_path, 'wb') as file:
+            file.write(response.content)
+
+
 def download(URL: str, OUTPUT_DIR: str) -> str:
     try:
         response = requests.get(URL)  # create HTTP response object
@@ -37,31 +53,19 @@ def download(URL: str, OUTPUT_DIR: str) -> str:
         raise ValueError('HTTP error occurred: {}'.format(http_err))
     except Exception as err:
         raise ValueError('Other error occurred: {}'.format(err))
-    else:
-        content = response.content
-        soup = BeautifulSoup(content, 'html.parser')
-        resulting_file_name = format_url(URL, 'file')
-        complete_path = os.path.join(OUTPUT_DIR, resulting_file_name)
-        image_tags = soup.findAll('img')
 
-        if image_tags:
-            dir_name = format_url(URL, 'dir')
-            dir_full_path = os.path.join(OUTPUT_DIR, dir_name)
-            if not os.path.exists(dir_full_path):
-                os.mkdir(dir_full_path)
+    content = response.content
+    soup = BeautifulSoup(content, 'html.parser')
+    resulting_file_name = format_url(URL, 'file')
+    complete_path = os.path.join(OUTPUT_DIR, resulting_file_name)
+    image_tags = soup.findAll('img')
 
-            for image_link in image_tags:
-                link = urljoin(URL, image_link.get('src'))
-                response = requests.get(link)
-                image_name = format_url(link, 'file')
-                full_file_path = '{}/{}'.format(dir_full_path, image_name)
-                image_link['src'] = full_file_path
-                with open(full_file_path, 'wb') as file:
-                    file.write(response.content)
+    if image_tags:
+        download_img(URL, OUTPUT_DIR, image_tags)
 
-        try:
-            with open(complete_path, "w", encoding='utf-8') as file:
-                file.write(str(soup.prettify()))
-            return complete_path
-        except OSError:
-            raise ValueError("Directory is not available.")
+    try:
+        with open(complete_path, "w", encoding='utf-8') as file:
+            file.write(str(soup.prettify()))
+        return complete_path
+    except OSError:
+        raise ValueError("Directory is not available.")
