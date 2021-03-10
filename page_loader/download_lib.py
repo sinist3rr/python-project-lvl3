ac57f3572c5ed86ba72511a0c22a51f91c03bf9b
sinist3rr <darkfloki@protonmail.com>
2021-03-10 16:27:59 +0200

    add more resourses

--- page_loader/download_lib.py
+++ page_loader/download_lib.py
@@ -22,9 +22,12 @@ def download(URL: str, OUTPUT_DIR: str) -> str:
     resulting_file_name = format_url(URL, 'file')
     complete_path = os.path.join(OUTPUT_DIR, resulting_file_name)
     image_tags = soup.findAll('img')
+    link_tags = soup.findAll('link')
+    script_tags = soup.findAll('script')
 
-    if image_tags:
-        download_img(URL, OUTPUT_DIR, image_tags)
+    download_res(URL, OUTPUT_DIR, image_tags)
+    download_res(URL, OUTPUT_DIR, link_tags, 'href')
+    download_res(URL, OUTPUT_DIR, script_tags)
 
     try:
         with open(complete_path, "w", encoding='utf-8') as file:
@@ -34,21 +37,24 @@ def download(URL: str, OUTPUT_DIR: str) -> str:
         raise ValueError("Directory is not available.")
 
 
-def download_img(URL: str, OUTPUT_DIR: str, image_tags: list):
+def download_res(URL: str, OUTPUT_DIR: str, tags: list, location: str = 'src'):
     dir_name = format_url(URL, 'dir')
     dir_full_path = os.path.join(OUTPUT_DIR, dir_name)
     if not os.path.exists(dir_full_path):
         os.mkdir(dir_full_path)
 
-    for image_link in image_tags:
-        link = urljoin(URL, image_link.get('src'))
-        response = requests.get(link)
-        image_name = format_url(link, 'file')
-        full_file_path = '{}/{}'.format(dir_full_path, image_name)
-        img_file_path = '{}/{}'.format(dir_name, image_name)
-        image_link['src'] = img_file_path
-        with open(full_file_path, 'wb') as file:
-            file.write(response.content)
+    for tag in tags:
+        if check_domain(URL, tag.get(location)):
+            link = urljoin(URL, tag.get(location))
+            response = requests.get(link)
+            res_name = format_url(link, 'file')
+            full_file_path = '{}/{}'.format(dir_full_path, res_name)
+            res_file_path = '{}/{}'.format(dir_name, res_name)
+            tag[location] = res_file_path
+            with open(full_file_path, 'wb') as file:
+                file.write(response.content)
+        else:
+            continue
 
 
 def format_url(url: str, out_type: str) -> str:
@@ -70,3 +76,15 @@ def format_url(url: str, out_type: str) -> str:
 def replace_to_dash(url: str) -> str:
     pattern = re.compile('[^a-zA-Z0-9]')
     return re.sub(pattern, '-', url)
+
+
+def check_domain(base_url: str, resource_url: str) -> bool:
+    main_domain = urlparse(base_url).hostname
+    resource_domain = urlparse(resource_url).hostname
+
+    if resource_domain is None or main_domain is None:
+        return True
+    elif resource_domain.endswith(main_domain):
+        return True
+    else:
+        return False
