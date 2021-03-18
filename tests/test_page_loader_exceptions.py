a97af8efe0998e2f5ec23c1bd734b002f92d5ef0
sinist3rr <darkfloki@protonmail.com>
2021-03-18 14:54:20 +0200

    add exception test

---
+++ tests/test_page_loader_exceptions.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+"""Various page_loader tests."""
+
+import pytest
+import requests
+import tempfile
+import os
+import page_loader
+
+
+def test_page_loader():
+    with pytest.raises(ValueError):
+        page_loader.download('http://example.com', '/tmp1')
