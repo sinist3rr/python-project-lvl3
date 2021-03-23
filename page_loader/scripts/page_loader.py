ca391ed45e4b918b22e0cd989459473fb62bf913
sinist3rr <darkfloki@protonmail.com>
2021-03-23 15:58:48 +0200

    fix exception error

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -5,6 +5,7 @@ import sys
 from page_loader.logger_config import generate_logger_config
 from page_loader import download
 from page_loader.cli import prompt_args
+from page_loader.errors import AppInternalError
 
 
 def main():
@@ -21,7 +22,7 @@ def main():
     try:
         print(download(args.url, args.output))
         sys.exit(0)
-    except ValueError:
+    except AppInternalError:
         sys.exit(1)
 
 
