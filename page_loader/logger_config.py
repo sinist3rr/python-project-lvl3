f21e13cf20bcb6455b8e2affc6f2b0a99d7f8dd7
sinist3rr <darkfloki@protonmail.com>
2021-04-08 14:29:37 +0300

    fix align

--- page_loader/logger_config.py
+++ page_loader/logger_config.py
@@ -20,8 +20,8 @@ def generate_logger_config(level: int) -> dict:
             'std_format': {
                 'format': '{asctime} - {name} - {levelname} - {module}:{funcName}- {message}',  # noqa: E501
                 'style': '{'
-                }
-            },
+            }
+        },
         'handlers': {
             'console_out': {
                 'class': 'logging.StreamHandler',
@@ -29,24 +29,24 @@ def generate_logger_config(level: int) -> dict:
                 'stream': 'ext://sys.stdout',
                 'level': 'DEBUG',
                 'formatter': 'std_format'
-                },
+            },
             'console_err': {
                 'class': 'logging.StreamHandler',
                 'stream': 'ext://sys.stderr',
                 'level': 'ERROR',
                 'formatter': 'std_format'
             }
-            },
+        },
         'loggers': {
             'page_loader.download_lib': {
                 'level': level,
                 'handlers': ['console_out', 'console_err']
                 # 'propagate': False
-                },
+            },
             'urllib3': {
                 'level': level,
                 'handlers': ['console_out', 'console_err']
             }
-            },
-        }
+        },
+    }
     return logger_config
