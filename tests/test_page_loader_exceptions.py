b97ad0445b0ae55fff259f2e5fb64d8c63b2ec23
sinist3rr <darkfloki@protonmail.com>
2021-04-16 09:41:05 +0300

    use mocking

--- tests/test_page_loader_exceptions.py
+++ tests/test_page_loader_exceptions.py
@@ -4,11 +4,16 @@
 
 import pytest
 import page_loader
+import requests
 from page_loader.errors import FileError, NetworkError
 
 
-def test_page_loader():
+def test_page_loader(requests_mock):
+    url_normal = 'http://example.com'
+    url_abnormal = 'http://unexisting.com'
+    requests_mock.get(url_normal)
+    requests_mock.get(url_abnormal, exc=requests.exceptions.ConnectTimeout)
     with pytest.raises(FileError):
-        page_loader.download('http://example.com', '/tmp1')
+        page_loader.download(url_normal, '/tmp1')
     with pytest.raises(NetworkError):
-        page_loader.download('https://httpbin11.org/', '/tmp')
+        page_loader.download(url_abnormal, '/tmp1')
