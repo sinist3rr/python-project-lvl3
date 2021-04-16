4687bdbb9fec6bd297ff7647715db26e32bf0f12
sinist3rr <darkfloki@protonmail.com>
2021-04-16 09:21:13 +0300

    use mocking

--- tests/test_downloader_status_codes.py
+++ tests/test_downloader_status_codes.py
@@ -8,23 +8,15 @@ import page_loader
 from page_loader.errors import NetworkError
 
 
-@pytest.mark.parametrize(
-    'url,result',
-    [
-        (
-            'https://httpbin.org/status/403',
-            403
-        ),
-        (
-            'https://httpbin.org/status/404',
-            404
-        ),
-        (
-            'https://httpbin.org/status/500',
-            500
-        )]
-)
-def test_downloader_codes(url, result):
+def test_downloader_codes(requests_mock):
+    error_403 = 'https://httpbin.org/status/403'
+    error_404 = 'https://httpbin.org/status/404'
+    error_500 = 'https://httpbin.org/status/500'
+    requests_mock.get(error_403, text='Forbidden', status_code=403)
+    requests_mock.get(error_404, text='Not Found', status_code=404)
+    requests_mock.get(error_500, text='Internal Server Error', status_code=500)
     with tempfile.TemporaryDirectory() as tmp_dir_name:
         with pytest.raises(NetworkError):
-            page_loader.download(url, tmp_dir_name)
+            page_loader.download(error_403, tmp_dir_name)
+            page_loader.download(error_404, tmp_dir_name)
+            page_loader.download(error_500, tmp_dir_name)
