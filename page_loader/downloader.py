6b16c0e3288bdeeed8df4633e6d55d1ff92bf186
sinist3rr <darkfloki@protonmail.com>
2021-04-23 11:43:49 +0300

    rename variable

--- page_loader/downloader.py
+++ page_loader/downloader.py
@@ -9,13 +9,13 @@ logger = logging.getLogger(__name__)
 
 
 def download(url: str, output_dir: str) -> str:
-    webpage_content = page_loader.resources.get_http(url)
-    logger.debug('Full http response body %s', webpage_content)
+    page = page_loader.resources.get_http(url)
+    logger.debug('Full http response body %s', page)
 
     resulting_file_name = page_loader.urls.format_file_url(url)
     complete_path = os.path.join(output_dir, resulting_file_name)
 
-    soup, all_tags = page_loader.parsers.parse_resource_tags(webpage_content)
+    soup, all_tags = page_loader.parsers.parse_resource_tags(page)
     local_tags = page_loader.parsers.get_local_resource_tags(all_tags, url)
 
     resource_urls = page_loader.parsers.get_urls(local_tags, url)
